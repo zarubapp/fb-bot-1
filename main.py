31ad0b274528aab284d39bd477c6cbb59f3b2bb8
vlad <zarubovpetr@gmail.com>
2021-12-25 10:42:49 +0100

    update

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import telebot
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+bot = telebot.TeleBot('2068029951:AAHYVHIqwQpl03eLS1w8HeZFm4eCTN5X1-s')
 
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.polling(none_stop=True, interval=0)
